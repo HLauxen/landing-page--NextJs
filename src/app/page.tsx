4778976227cb87d53d9b21411e993cc38df4a13d
Henrique Lauxen Seefeld <henrique.seefeld@gmail.com>
2023-07-17 14:15:05 -0300

    page commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import ImageForm from '../assets/Link/Camada-12.jpg'
 import ImageHome from '../assets/Link/home.png'
 import ImageLink from '../assets/Link/link.png'
 import { useRouter } from "next/navigation";
+import { GithubLogo, LinkedinLogo } from 'phosphor-react'
 
 export default function App() {
   const router = useRouter();
@@ -17,20 +18,27 @@ export default function App() {
     router.push('/home')
   }
   const routerLink = () => {
-    router.push('/link')
+    router.push('https://www.linkedin.com/in/henrique-lauxen-seefeld-27009426b/')
+  }
+  const routerGit= () => {
+    router.push('https://github.com/HLauxen')
   }
   
   
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <h1 className={styles.name}>Nome Teste</h1>
+        <h1 className={styles.name}>Henrique Lauxen</h1>
         <p className={styles.title}>Links Úteis:</p>
         <div className={styles.links}>
           
-          <button  onClick={routerForm} className={styles.link}><Image alt='imagem'src={ImageForm} width={300} height={125}/></button>
           <button onClick={routerHome} className={styles.link}><Image alt='imagem' src={ImageHome} width={300} height={125}/></button>
-          <button  onClick={routerLink} className={styles.link}><Image alt='imagem' src={ImageLink} width={300} height={125}/></button>
+          <button  onClick={routerForm} className={styles.buttonDisabled}>Contratar</button>
+          <div className={styles.link__logo}>
+            <button  onClick={routerLink} className={styles.logo}><LinkedinLogo size={28} /></button>
+            <button  onClick={routerGit} className={styles.logo}><GithubLogo  size={28} /></button>
+          </div>
+          
   
         </div>
       </div>
